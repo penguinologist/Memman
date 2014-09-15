32b2c1492197656bd1578b53d8d6946097d4bdf3
jgem12345 <Megaman12345>
2014-09-15 14:11:38 -0400

    nc

--- Hash.java
+++ Hash.java
@@ -1,40 +1,123 @@
-package Backend;
-
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 public class Hash<K, V> implements Map<K, V>, Cloneable, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int size;
-
 	private int capacity;
-	private Entry[] table;
+	private int[] table;
+
+	/*
+	 * 
+	 * data is stored in memory manager entry table is the list of all entries
+	 * (hash values)
+	 */
 
 	public Hash(int initialCapacity) {
 		capacity = 1;
 		while (capacity < initialCapacity)
 			capacity *= 2;
 
-		table = new Entry[capacity];
+		table = new int[capacity];
+	}
+
+	private void extendArray() {
+		int newCapacity = 2 * capacity;
+		int[] newTable = new int[newCapacity];
+		for (int i = 0; i < capacity; i++) {
+			newTable[i] = table[i];
+		}
+		capacity = newCapacity;
+		size = newCapacity;
+		table = newTable;
 
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
+		int[] tab = table;
+		for (int i = 0; i < tab.length; i++)
+			tab[i] = -1;
+
+		table = tab;
 		size = 0;
 	}
 
 	@Override
+	public V put(K key, V value) {
+		// original java source code
+		//
+		// if (key == null)
+		// return putForNullKey(value);
+		// int hash = hash(key.hashCode());
+		// int i = indexFor(hash, table.length);
+		// for (Entry<k, V> e = table[i]; e != null; e = e.next) {
+		// Object k;
+		// if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+		// V oldValue = e.value;
+		// e.value = value;
+		// e.recordAccess(this);
+		// return oldValue;
+		// }
+		// }
+		// modCount++;
+		// addEntry(hash, key, value, i);
+
+		if (key == null) {
+			// do something
+		}
+
+		int hash = (int) sfold((String) value, size);
+
+		return null;
+
+	}
+
+	// Use folding on a string, summed 4 bytes at a time
+	private long sfold(String s, int M) {
+		int intLength = s.length() / 4;
+		long sum = 0;
+		for (int j = 0; j < intLength; j++) {
+			char c[] = s.substring(j * 4, (j * 4) + 4).toCharArray();
+			long mult = 1;
+			for (int k = 0; k < c.length; k++) {
+				sum += c[k] * mult;
+				mult *= 256;
+			}
+		}
+
+		char c[] = s.substring(intLength * 4).toCharArray();
+		long mult = 1;
+		for (int k = 0; k < c.length; k++) {
+			sum += c[k] * mult;
+			mult *= 256;
+		}
+
+		return (Math.abs(sum) % M);
+	}
+
+	@Override
 	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
+		int k = (int) key;
+		for (int i = 0; i < table.length; i++) {
+			if (table[i] == k) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
 	@Override
 	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
+
+		// not implemented
 		return false;
 	}
 
@@ -44,33 +127,30 @@ public class Hash<K, V> implements Map<K, V>, Cloneable, Serializable {
 		return null;
 	}
 
+	// key = hash value
 	@Override
 	public V get(Object key) {
-		// TODO Auto-generated method stub
+		if (key == null) {
+			// do something
+		}
+
 		return null;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
 
 	@Override
 	public Set<K> keySet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public V put(K key, V value) {
-		// TODO Auto-generated method stub
+		// not implemented
 		return null;
 	}
 
 	@Override
 	public void putAll(Map<? extends K, ? extends V> m) {
-		// TODO Auto-generated method stub
+		// not implemented
 
 	}
 
@@ -88,7 +168,7 @@ public class Hash<K, V> implements Map<K, V>, Cloneable, Serializable {
 
 	@Override
 	public Collection<V> values() {
-		// TODO Auto-generated method stub
+		// not implemented
 		return null;
 	}
 
--- Memman.java
+++ Memman.java
@@ -2,11 +2,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// -------------------------------------------------------------------------
+/**
+ * Write a one-sentence summary of your class here. Follow it with additional
+ * details about its purpose, what abstraction it represents, and how to use it.
+ * 
+ * @author1 Phuong Le(Ldp91)
+ * @version 2014.09.14
+ */
 public class Memman {
 	private static int initialHashSize;
 	private static int blockSize;
 	private static String commandFile;
+	private static Hash<String, Integer> artists;
+	private static Hash<String, Integer> songs;
+	private static MemoryManager memManager;
 
+	// ----------------------------------------------------------
+	/**
+	 * Place a description of your method here.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		if (args.length != 3) {
 			return;
@@ -15,11 +32,21 @@ public class Memman {
 		initialHashSize = Integer.parseInt(args[0]);
 		blockSize = Integer.parseInt(args[1]);
 		commandFile = args[2];
-
-		MemoryManager memManager = new MemoryManager();
+		artists = new Hash<String, Integer>(initialHashSize);
+		songs = new Hash<String, Integer>(initialHashSize);
+		memManager = new MemoryManager(blockSize, initialHashSize); // TODO
+																	// should
+																	// we link
 		readCommandFile(commandFile);
+
 	}
 
+	// ----------------------------------------------------------
+	/**
+	 * Place a description of your method here.
+	 * 
+	 * @param fileName
+	 */
 	public static void readCommandFile(String fileName) {
 		Scanner inStream = null;
 
@@ -30,11 +57,11 @@ public class Memman {
 		}
 		while (inStream.hasNext()) {
 			String cmdLine = inStream.nextLine();
-			eval(cmdLine);
+			parseLine(cmdLine);
 		}
 	}
 
-	private static void eval(String cmdLine) {
+	private static void parseLine(String cmdLine) {
 		cmdLine = cmdLine.trim();
 		String command = cmdLine.substring(0, cmdLine.indexOf(' '));
 		cmdLine = cmdLine.substring(cmdLine.indexOf(' ') + 1).trim();
@@ -62,14 +89,63 @@ public class Memman {
 	}
 
 	private static void insert(String[] args) {
+		boolean success = false;
+
+		if (args[0] != null && args[1] != null) {
+			success = true;
+
+			// artist <SEP> song
+
+			if (!artists.containsKey(args[0])) {
+
+				artists.put(args[0], memManager.insert(args[0].getBytes()));
+			}
+			if (!songs.containsKey(args[1])) {
+				songs.put(args[1], memManager.insert(args[1].getBytes()));
+			}
+
+		}
+
+		if (success) {
+			System.out.println("Successful execution.");
+		} else {
+			System.out.println("Failed execution.");
+		}
 
 	}
 
 	private static void remove(String[] args) {
+		boolean actionExecuted = false;
+		if (artists.containsKey(args[0])) {
+			artists.remove(args[0]);
+			actionExecuted = true;
+		}
+		if (songs.containsKey(args[0])) {
+			songs.remove(args[0]);
+			actionExecuted = true;
+		}
+
+		if (actionExecuted) {
+			System.out.println("Successful execution.");
+		} else {
+			System.out.println("Failed execution.");
+		}
 
 	}
 
 	private static void print(String[] args) {
+		if (args[0].equals("artist")) {
+			//print out values
+			//iterate this stuff...{
+			
+			memManager.getData(artists.get(args[0]));
+			
+			//}
+			
+		} else if (args[0].equals("song")) {
+			memManager.getData(songs.get(args[0]));
+		} else if (args[0].equals("blocks")) {
 
+		}
 	}
 }
